9882c955cca26c46f15c9739ffc38dbafdf340d3
Mohit yadav <123181137+Mohityadav3036@users.noreply.github.com>
2023-12-12 17:19:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnpopup = document.querySelector('.btnlogin-popup');
+registerlink.addEventListener('click', ()=> {
+  wrapper.classList.add('active');
+});
+const iconclose = document.querySelector('.icon-close');
+loginlink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+  });
+
+  btnpopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+  });
+
+  iconclose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+  });
